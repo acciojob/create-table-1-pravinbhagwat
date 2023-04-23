415a00070c2cff14ca58637a7eacd4ea08f466c4
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-04-23 19:44:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
+  let table = document.getElementById('sampleTable');
+
+  // insert new row at top of table
+  let newRow = table.insertRow(0);
+
+  // insert two new cells into row
+  let leftCell = newRow.insertCell(0);
+  let rightCell = newRow.insertCell(1);
+
+  // set text content of new cells
+  leftCell.textContent = 'New Cell1';
+  rightCell.textContent = 'New Cell2';
   
 }
